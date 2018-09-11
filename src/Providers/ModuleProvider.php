ee84f1ad64ecbe456d43aaae50d417a569421e84
Samuel De Backer <sdebacker@gmail.com>
2018-09-11 18:21:23 +0200

    SitemapServiceProvider removed

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -8,12 +8,6 @@ class ModuleProvider extends ServiceProvider
 {
     public function register()
     {
-
-        /*
-         * Register sitemap package
-         */
-        $this->app->register('Roumen\Sitemap\SitemapServiceProvider');
-
         /*
          * Register route service provider
          */
