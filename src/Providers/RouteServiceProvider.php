7ef946f4f0f62819cfa912a9809a45e8e1f09356
Samuel De Backer <sdebacker@gmail.com>
2020-09-17 23:52:23 +0200

    Callable syntax for controller route declarations.

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -5,18 +5,10 @@ namespace TypiCMS\Modules\Sitemap\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use TypiCMS\Modules\Sitemap\Http\Controllers\PublicController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to the controller routes in your routes file.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'TypiCMS\Modules\Sitemap\Http\Controllers';
-
     /**
      * Define the routes for the application.
      */
@@ -26,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
             /*
              * Front office routes
              */
-            $router->get('sitemap.xml', 'PublicController@generate')->name('sitemap');
+            $router->get('sitemap.xml', [PublicController::class, 'generate'])->name('sitemap');
         });
     }
 }
