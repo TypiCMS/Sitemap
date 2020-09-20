bfa33cf4c2107150b7175becc71f3c6c8271c62d
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 22:40:47 +0200

    Routes definition cleaned

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -3,7 +3,6 @@
 namespace TypiCMS\Modules\Sitemap\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use TypiCMS\Modules\Sitemap\Http\Controllers\PublicController;
 
@@ -14,11 +13,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        Route::namespace($this->namespace)->group(function (Router $router) {
-            /*
-             * Front office routes
-             */
-            $router->get('sitemap.xml', [PublicController::class, 'generate'])->name('sitemap');
-        });
+        /*
+         * Front office routes
+         */
+        Route::get('sitemap.xml', [PublicController::class, 'generate'])->name('sitemap');
     }
 }
